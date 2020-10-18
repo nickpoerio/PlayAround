7fab3e87deb15d57bae8428190652f9a93f70fb3
LeonardoPoerio <leonardo.poerio@gmail.com>
2020-10-18 16:49:36 +0200

    first draft of main function

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+PlayAround games
+
+Authors: Nicola Poerio & Leonardo Poerio
+Date: 18th October 2020
+
+"""
+
+game_name = "BlowBall"
+interactive = True
+n_players = 1
+
+
+print("Game {} initialized".format(game_name))
+print("Interactive mode: {}".format(interactive))
+print("Number of players: {}".format(n_players))
+
+
